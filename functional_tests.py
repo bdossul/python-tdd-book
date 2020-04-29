582084b9a5a6d1ef13ef3c7b5c214ad22240a8d3
makoseafox <makoseafox@bakhtiars-air.attlocal.net>
2020-04-29 12:47:24 -0400

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('Things to do', self.browser.title)
-        header_text = self.browser.find.element_by_tag_name('h1').text
+        header_text = self.browser.find_element_by_tag_name('h1').text
 
         self.assertIn('Things to do', header_text)
 
@@ -44,8 +44,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows)
-        
+            any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
