a453247f80f8c3e47ded54692a73d934a445a83a
Bakhtiar Dossul <bdossul@gmail.com>
2020-05-09 17:15:21 -0400

    Add a fabfile for automated deploys

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,56 @@
+import random
+from fabric.contrib.files import append, exists
+from fabric.api import cd, env, local, run
+
+REPO_URL = 'https://github.com/bdossul/python-tdd-book.git'  
+
+#Breakdown of a Fabric Script for Our Deployment
+def deploy():
+    site_folder = f'/home/makopeth/sites/superlists.bdienterprises.com'  
+    run(f'mkdir -p {site_folder}')  
+    with cd(site_folder):  
+        _get_latest_source()
+        _update_virtualenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
+
+#Pulling Down Our Source Code with Git
+def _get_latest_source():
+    if exists('.git'):  
+        run('git fetch')  
+    else:
+        run(f'git clone {REPO_URL} .')  
+    current_commit = local("git log -n 1 --format=%H", capture=True)  
+    run(f'git reset --hard {current_commit}')  
+
+
+#Updating the Virtualenv
+
+def _update_virtualenv():
+    if not exists('virtualenv/bin/pip'):  
+        run(f'python3.6 -m venv virtualenv')
+    run('./virtualenv/bin/pip install -r requirements.txt') 
+
+#Creating a New .env File if Necessary
+
+def _create_or_update_dotenv():
+    append('.env', 'DJANGO_DEBUG_FALSE=y')  
+    append('.env', f'SITENAME={env.host}')
+    current_contents = run('cat .env')  
+    if 'DJANGO_SECRET_KEY' not in current_contents:  
+        new_secret = ''.join(random.SystemRandom().choices(  
+            'abcdefghijklmnopqrstuvwxyz0123456789', k=50
+        ))
+        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+
+#Updating Static Files
+
+def _update_static_files():
+    run('./virtualenv/bin/python manage.py collectstatic --noinput') 
+
+#Migrating the Database If Necessary
+
+def _update_database():
+    run('./virtualenv/bin/python manage.py migrate --noinput')
+
